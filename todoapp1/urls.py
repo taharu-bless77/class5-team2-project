0f296516275f994b1f67d619eb6771c984dc8998
taharu-bless77 <167529955+taharu-bless77@users.noreply.github.com>
2024-12-13 01:25:48 +0900

    ユーザーからのリクエストに応じた関数の定義

--- todoapp1/urls.py
+++ todoapp1/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import add_task_view
 
 urlpatterns=[
     path('',views.index,name='index'),
     path('tuika',views.add_task,name ='tuika'),
     path('from hiduke to task',views.get_tasks_by_date, name='from hiduke to task'),
+    path('add_task/', add_task_view, name='add_task'),  # タスク追加用のURL
+    
 ]
--- todoapp1/views.py
+++ todoapp1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -14,4 +14,12 @@ def add_task(date, task):
 def get_tasks_by_date(date):
     # 日付に基づいてタスクを取得する関数
     return [task for task_date, task in tasks if task_date == date]
+
+def add_task_view(request):
+    if request.method == 'POST':#フォームが送信されたとき（POSTリクエスト）の処理を行います。
+        date = request.POST.get('date')  # フォームから日付を取得
+        task = request.POST.get('task')  # フォームからタスクを取得
+        add_task(date, task)  # タスクを追加
+        return redirect('index')  # タスク追加後にインデックスページにリダイレクト
+    return render(request, 'todoapp1/index.html')  # GETリクエストの場合はフォームを表示
 
