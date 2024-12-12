60580b100354644b9c37db2822f3601b0b94e375
taharu-bless77 <167529955+taharu-bless77@users.noreply.github.com>
2024-12-13 00:56:32 +0900

    urlsのリストの追加

--- todoapp1/urls.py
+++ todoapp1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('tuika',views.add_task,name ='tuika'),
+    path('from hiduke to task',views.get_tasks_by_date, name='from hiduke to task'),
 ]
--- todoapp1/views.py
+++ todoapp1/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'todoapp1/index.html')
+
+tasks = []
+
+def add_task(date, task):
+    # タスクを追加する関数
+    tasks.append([date, task])  # 日付とタスクをリストに追加
+
+def get_tasks_by_date(date):
+    # 日付に基づいてタスクを取得する関数
+    return [task for task_date, task in tasks if task_date == date]
+
