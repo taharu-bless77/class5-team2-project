95f9b506c502b605eee346e4b3825ce5268a17d8
s1f10240154 <s1f102401540@iniad.org>
2024-12-13 15:49:25 +0900

    create model
    Articleモデルの作成

--- todoapp1/models.py
+++ todoapp1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    task=models.CharField(max_length=200)
+    detail=models.TextField()
+
+    def register(self):
+        self.save()
+
+    def __str__(self):
+        return self.task
